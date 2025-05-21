3f717bc64719195af9e7ca50a4ba5917aae7b5d7
Jai2992 <jaiguru.l2022ai-ml@sece.ac.in>
2025-05-21 07:55:42 +0530

    added dynamic port allocation and connection error handling

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import psutil
 import serial
 import time
 import logging
-
-PORT = 'COM7'
-BAUD = 9600
-
-ser = serial.Serial(PORT, BAUD, timeout=1)
-time.sleep(2) 
+import serial.tools.list_ports
 
 logging.basicConfig(
     filename='app.log',
@@ -15,6 +10,32 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+# find the port of esp8266 dynamically
+def find_port():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        if "CH340" in port.description:
+            return port.device
+    return None
+
+# connect the esp8266 to the device 
+PORT = find_port()
+BAUD = 9600
+if PORT:
+    try:
+        ser = serial.Serial(PORT, BAUD, timeout=1)
+        time.sleep(2) 
+        print("connected to PORT device",PORT)
+        logging.info(f"device connected sucessfully through port {PORT}")
+    except serial.SerialException as e:
+        print("error in connecting device")
+        ser = None
+else:
+    print("device not found connect device")
+    logging.error("device not found")
+    exit(1)
+
 
 # sends command to esp8266
 def send_command(cmd):
@@ -32,6 +53,7 @@ def get_battery_percent():
     return [battery.percent,battery.power_plugged]
 
 if __name__ == '__main__':
+    print("app started sucessfully")
     while True:
         bat = get_battery_percent()
         if bat[0]>90 and bat[1]:
@@ -39,3 +61,4 @@ if __name__ == '__main__':
         elif bat[0]<20 and bat[1] == False:
             send_command("on")
         time.sleep(1)
+
